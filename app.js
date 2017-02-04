3488642bb96c174030ca227c5dc5f85df64dd1b4
trig010 <trig.code@gmail.com>
2017-02-03 22:42:33 -0600

    final commit

--- app.js
+++ app.js
@@ -2,47 +2,41 @@
 let state = {
     items: []
 }
+
 //build functions that modify state
 const addItem = function(state, item) {
     state.items.push(item)
+    renderList(state, $('.shopping-list'))
 }
+//**was building state management here**
 // const toggleChecked = function(state, item) {
 //     state.items.item.checked = true;
 // }
 // const toggleDeleted = function(item) {
 //     state.items.item.deleted = true;
 // }
+
 //build functions that render state
 const renderList = function(state, element) {
-    console.log(state)
-    const itemsHTML = $('.shopping-list').state.items.map(function(item) {
-        return $('.shopping-item').after(item)
+    let itemsHTML = state.items.map(function(item, index) {
+        console.log(item)
+        return '<li>\n\t<span class="shopping-item">' + item + '</span>\n<div class="shopping-item-controls">\n\t<button class="shopping-item-toggle">\n\t<span class="button-label">check</span>\n</button>\n<button class="shopping-item-delete">\n\t<span class="button-label">delete</span>\n</button>\n</div></li>'
     })
-    element.html(itemsHTML)
+    return element.html(itemsHTML)
 }
+
 //build event listeners
-function submitForm() {
-    $('#shopping-list-entry').submit(function(event) {
-        event.preventDefault();
-        console.log('HELLLLOOO')
-        // addItem(state, $('input').val())
-        // console.log(state)
+//**DO NOT NEED TO BE IN FUNCTIONS**
+$('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    addItem(state, $('input').val())
+    console.log(state)
+})
 
-    })
-    // renderList(state, $('.shopping-list'))
-}
+$('.shopping-list').on('click', '.shopping-item-toggle', function() {
+    $(this).closest('li').toggleClass('shopping-item__checked')
+})
 
-function checkItem() {
-    $('.shopping-item-toggle').click(function() {
-        $( this ).closest('li').toggleClass('shopping-item__checked')
-    })
-}
-
-function deleteItem() {
-    $('.shopping-item-delete').click(function() {
-        $( this ).closest('li').remove()
-    })
-}
-
-deleteItem()
-checkItem()
+$('.shopping-list').on('click', '.shopping-item-delete', function() {
+$(this).closest('li').remove()
+})
